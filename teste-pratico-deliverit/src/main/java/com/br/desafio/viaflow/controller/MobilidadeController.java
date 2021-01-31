7b981f8f0985a4ba36dd63f9d7e4c1e6ee097ce7
gianlucampos <gianlucampos97@gmail.com>
2021-01-31 17:58:59 -0300

    Usando ResponseEntity para retornar status ao executar uma requisição

--- teste-pratico-deliverit/src/main/java/com/br/desafio/viaflow/controller/MobilidadeController.java
+++ teste-pratico-deliverit/src/main/java/com/br/desafio/viaflow/controller/MobilidadeController.java
@@ -1,17 +1,15 @@
 package com.br.desafio.viaflow.controller;
 
-import com.br.desafio.viaflow.dto.ItinerarioDTO;
-import com.br.desafio.viaflow.model.LinhaTransporte;
 import com.br.desafio.viaflow.service.LinhaTransporteService;
 import com.br.desafio.viaflow.service.client.MobilidadeClient;
 import java.util.HashMap;
-import java.util.List;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -26,22 +24,43 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 public class MobilidadeController {
 
     @Autowired
-    private LinhaTransporteService service;
+    private LinhaTransporteService serviceLinhaTransporte;
 
-    @GetMapping()
-    public ItinerarioDTO init() {
+    @GetMapping
+    public ResponseEntity init() {
+//        service.importLinhasTranporte();
+        return listLinhasTransporte();
+    }
+
+    @GetMapping(path = "linhas")
+    public ResponseEntity listLinhasTransporte() {
+        if (serviceLinhaTransporte.listAll().isEmpty()) {
+            return new ResponseEntity(HttpStatus.NO_CONTENT);
+        }
+        return ResponseEntity.ok(serviceLinhaTransporte.listAll());
+    }
+
+    @GetMapping(path = "linhas/import")
+    public ResponseEntity testaImportLinhas() {
         try {
             MobilidadeClient cliente = new MobilidadeClient();
-            return cliente.buscaItinerario();
+            return ResponseEntity.ok(cliente.buscaLinhasTransporte());
         } catch (Exception ex) {
             ex.printStackTrace();
-            return null;
+            return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
-    
-    @GetMapping(path = "linhas")
-    public List<LinhaTransporte> listLinhasTransporte() {
-        return service.listAll();
+
+    @GetMapping(path = "itinerario/import")
+    public ResponseEntity testaImportItinerario() {
+        try {
+            MobilidadeClient cliente = new MobilidadeClient();
+            return ResponseEntity.ok(cliente.buscaItinerario());
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+
+        }
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
